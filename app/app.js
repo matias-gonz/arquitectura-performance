0c62d4beafad58ba1501a2adbdfdfaa20265c057
matias-gonz <maigonzalez@fi.uba.ar>
2023-04-13 23:05:58 -0300

    Add app.js

---
+++ app/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+process.on('SIGTERM', async () => {
+  await redisClient.quit();
+});
+
+app.get('/ping', (req, res) => {
+  console.log('Request received at /ping');
+  res.status(200).send('Pong!');
+});
+
+app.get('/space_news', async (req, res) => {
+  console.log('Request received at /space_news');
+  let limit = 5;
+  const response = await axios.get('https://api.spaceflightnewsapi.net/v4/articles/?limit=' + limit);
+  let articles = response.data.results;
+  let titles = articles.map(article => article.title);
+  console.log(titles);
+  res.status(200).send(titles);
+});
+
+app.listen(3000, () => console.log("Listening at 5000"))
